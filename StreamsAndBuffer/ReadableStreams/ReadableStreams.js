32485750429da8403e397352187f0bfbac74edaa
PT2309 <sunny.palash@gmail.com>
2019-03-13 18:15:31 +0530

    Node classes

--- StreamsAndBuffer/ReadableStreams/ReadableStreams.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt');
-
-myReadStream.on('data', (dataBlock) => {
-  console.log('Data chunk incoming!!!!');
-  console.log(dataBlock);
-})
