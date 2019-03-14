92ff1c18f3e22617ecf305f659c74e1e18d83326
PT2309 <sunny.palash@gmail.com>
2019-03-15 01:22:31 +0530

    Node concepts: Serving JSON data, Streamable Concepts

--- StreamsAndBuffer/Pipes/Pipe.js
+++ StreamsAndBuffer/Pipes/Pipe.js
@@ -6,12 +6,7 @@ var myReadStream = fs.createReadStream(__dirname + '/readme.txt');
 // what we want is to write the recieved chunk to the stream.
 var myWriteableStream = fs.createWriteStream(__dirname + '/writeme.txt')
 
-myReadStream.on('data', (dataBlock) => {
-  console.log('Data chunk incoming!!!!');
-  // console.log(dataBlock);
-  myWriteableStream.write(dataBlock);
-
-})
+myReadStream.pipe(myWriteableStream);
 
 // Since we use these streams so often we have a another way of doing all this.
 // There is a better way of doing all of this and those things are called Pipes.
